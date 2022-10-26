07a676e62c64cacaa4a59498788c067cf06942eb
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-26 16:24:48 -0500

    initial commit

---
+++ src/main/java/HelloWorldServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+
+public class HelloWorldServlet extends HttpServlet {
+
+//Create a page that displays "Hello World!" when a user visits /hello
+
+//Bonus
+//Make the page say "Hello, <name>!" if name is passed as part of the query string (e.g. /hello?name=codeup), otherwise default to "Hello, World!".
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String name = req.getParameter("name");
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello, " + name +"!</h>");
+    }
+}
