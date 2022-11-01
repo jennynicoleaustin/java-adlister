686dc2d3aa9a2d9a2a687ad080d2a2495844c30d
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-01 15:05:26 -0500

    add mysqldoa

---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,30 @@
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads{
+
+private Connection connection = null;
+
+    public MySQLAdsDao(Config config) throws SQLException {
+        DriverManager.registerDriver(new Driver());
+        connection = DriverManager.getConnection(
+                config.getUrl(),
+                config.getUser(),
+                config.getPassword()
+        );
+    }
+
+    @Override
+    public List<Ad> all() {
+        return null;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        return null;
+    }
+}
